59b990952dc24af84544ea4aadb8f7ed56cc20c6
Lewis Kabui <lewis_maina@yahoo.com>
2015-09-18 02:02:28 +0300

    Added PUT and DELETE endpoints for '/uefa/teams/:club_name' paths

--- index.js
+++ index.js
@@ -52,12 +52,41 @@ teamsRoute.get(function(req, res) {
 //create a new route /uefa/teams/:club
 var singleTeamRoute = router.route("/teams/:club_name");
 
+//create endpoint /uefa/teams/:club - GET
 singleTeamRoute.get(function(req, res) {
 	Team.findById(req.params.club_name, function(err, team) {
 		if (err)
 			res.send(err);
 
 		res.json(team);
+	});
+});
+
+//create endpoint /uefa/teams/:club - PUT
+singleTeamRoute.put(function(req, res) {
+
+	Team.findById(req.params.club_name, function(err, team) {
+		if (err) 
+			res.send(err);
+
+		team.coach = req.body.coach;
+
+		team.save(function(err) {
+			if (err)
+				res.send(err);
+
+			res.json(team);
+		});
+	});
+});
+
+//create endpoint /uefa/teams/:club - DELETE
+singleTeamRoute.delete(function(req, res) {
+	Team.findByIdAndRemove(req.params.club_name, function(err) {
+		if (err)
+			res.send(err);
+
+		res.json({message: req.params.club_name + " has been removed from UEFA!"});
 	});
 });
 
