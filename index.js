a0a578f5f287cdd6562dce3616711029c5ee5401
Lewis Kabui <lewis_maina@yahoo.com>
2015-09-18 01:14:39 +0300

    Mongoose package installed

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var express = require("express"),
 		port = process.env.PORT || 6048,
 		router = express.Router();
 
+var mongoose = require("mongoose"),
+		Beer = require("./models/team");
+
+mongoose.connect("mongodb://admin:Propan3@ds042698.mongolab.com:42698/champions_league");
+
 router.get("/", function(req, res) {
 	res.json({message: "You are at the root path of the UEFA app."});
 })
